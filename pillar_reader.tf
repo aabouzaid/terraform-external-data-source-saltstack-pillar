69f021247211a3d106d48d212a89ad18dcff1609
AAbouZaid <ahmed.m@aabouzaid.com>
2018-08-02 23:14:07 +0200

    add tf file example of pillar_reader

---
+++ pillar_reader.tf
@@ -0,0 +1,22 @@
+#
+# Example of pillar_reader.py
+# A Python script to read from SaltStack Pillar, and return the value in Terraform as external data source.
+#
+
+# Get data from SaltStack Pillar.
+data "external" "pillar" {
+  program = ["python", "${path.module}/pillar_reader.py"]
+  query = {
+    pillar = "foo:bar"
+  }
+}
+
+# You can even make it a bit shorter.
+locals {
+  foo_bar = "${data.external.pillar.result}"
+}
+
+# Also use it as output if you want.
+output "foo_bar" {
+  value = "${local.foo_bar}"
+}
